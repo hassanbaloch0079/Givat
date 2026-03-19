080d468397507d6032669e1b85b62ac62595c0e2
Hassan Baloch <69975345+hassanbaloch0079@users.noreply.github.com>
2026-03-20 00:40:55 +0500

    Comment out gsap registration and animations
    Comment out gsap plugin registration and related animations.

--- givat-main.js
+++ givat-main.js
@@ -644,7 +644,7 @@ ease: "power4.out"
     });
   });
   
-  gsap.registerPlugin(ScrollTrigger);
+  /*gsap.registerPlugin(ScrollTrigger);
   document.querySelectorAll(".animate-inner-imgs-icon img").forEach((img, i) => {
     gsap.fromTo(img,
                 {
@@ -663,7 +663,7 @@ ease: "power4.out"
       }
     }
                );
-  });
+  }); */
   /*gsap.fromTo(".animate-header-imgs-icon img",
               {
     clipPath: 'circle(0% at 50% 50%)',
