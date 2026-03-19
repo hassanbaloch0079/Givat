c4e467e885595134a7caab9355feb587693af3b9
Hassan Baloch <69975345+hassanbaloch0079@users.noreply.github.com>
2026-03-20 00:43:42 +0500

    Refactor image animation selectors for GSAP
    Updated image animation selectors in givat-main.js.

--- givat-main.js
+++ givat-main.js
@@ -644,8 +644,8 @@ ease: "power4.out"
     });
   });
   
-  /*gsap.registerPlugin(ScrollTrigger);
-  document.querySelectorAll(".animate-inner-imgs-icon img").forEach((img, i) => {
+  gsap.registerPlugin(ScrollTrigger);
+  document.querySelectorAll(".animate-inner-imgs-icon-1 img").forEach((img, i) => {
     gsap.fromTo(img,
                 {
       clipPath: 'circle(0% at 50% 50%)',
@@ -663,7 +663,7 @@ ease: "power4.out"
       }
     }
                );
-  }); */
+  });
   /*gsap.fromTo(".animate-header-imgs-icon img",
               {
     clipPath: 'circle(0% at 50% 50%)',
