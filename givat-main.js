a196ce2dca1d09a00c1202b7e9142847f8d90828
Hassan Baloch <69975345+hassanbaloch0079@users.noreply.github.com>
2026-03-20 00:45:10 +0500

    Update givat-main.js

--- givat-main.js
+++ givat-main.js
@@ -645,7 +645,7 @@ ease: "power4.out"
   });
   
   gsap.registerPlugin(ScrollTrigger);
-  document.querySelectorAll(".animate-inner-imgs-icon-1 img").forEach((img, i) => {
+  document.querySelectorAll(".animate-inner-imgs-icon img").forEach((img, i) => {
     gsap.fromTo(img,
                 {
       clipPath: 'circle(0% at 50% 50%)',
